61f061c7389f029d46e710ad865cf086a19d6834
Alex Armstrong <alehandrof@gmail.com>
2017-09-23 11:43:32 +0300

    Add build script and source files

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd src
+cat _font.less _evernote.less _colors-common.less _default.less > ../dist/xander-evernote.less
+cat _font.less _omnifocus.less _default.less > ../dist/xander-omnifocus.less
+cat _font.less _zenburn.less _colors-common.less _default.less _dark.less > ../dist/xander-zenburn.less
+cd ../..
+ln -sf xander-taskpaper-styles/dist/* .
